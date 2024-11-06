03f0075a7f95e20aec89002703168018c93f44fd
jeeva <jeevasathappan24@gmail.com>
2024-11-06 23:59:49 +0530

    Defining email function

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 url = 'http://programmer100.pythonanywhere.com/tours/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-# Headers are used for specific web server that don't like script programs, so these help us showing the program as browser
+
+
+# Headers are used for specific web server that don't like script programs, so these helps here showing the program as browser
+
+
 def scrape(url):
-    '''Scrape the page source from the url'''
+    """Scrape the page source from the url"""
     response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
+
 
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
     value = extractor.extract(source)['tours']
     return value
 
-def send_mail(extract):
+
+def send_mail(raw_message):
+    host = 'smtp.gmail.com'
+    port = 465
+    username = 'jeevasathappan2000@gmail.com'
+    password = 'ypxn naqt nwau ghhx'
+    receiver = 'jeevasathappan2000@gmail.com'
+    context = ssl.create_default_context()
+    message = f"""\
+Subject: New Event on board
+
+{raw_message}
+"""
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print('Mail sent')
 
+
 def read():
-    with open('data.txt','r') as file:
+    with open('data.txt', 'r') as file:
         return file.read()
+
 
 def store(extracted):
     with open('data.txt', 'a') as file:
-        file.write(extracted +'\n')
+        file.write(extracted + '\n')
 
 
 if __name__ == '__main__':
@@ -37,5 +61,3 @@ if __name__ == '__main__':
         if extracted not in content:
             store(extracted)
             send_mail(extracted)
-
-
